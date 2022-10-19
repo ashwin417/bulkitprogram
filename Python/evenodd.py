4dca036a91c757ca4e9f4c51e2c0b7e26feab631
Ashwita Rajesh <106796143+AshwitaR@users.noreply.github.com>
2022-10-19 19:38:01 +0530

    Add files via upload

---
+++ Python/evenodd.py
@@ -0,0 +1,17 @@
+print('.'*20,'Hello there! This is Python program workout by Ashwita Rajesh:)','.'*20)
+print('*Lets see if your number is odd or even!*')
+x = int(input("Now Enter your number : "))
+if x%2 == 0 :
+    print('Well,',x," is an even number!")
+else :
+    print('Well,',x,"is an odd number!")
+print("*Now Lets see if your number is positive or negative!*")
+y = float(input('Enter another number:'))
+if y>0:
+    print('Its a positive number:)')
+elif y==0:
+    print('Zero! Its a positive number')
+else:
+    print('Its a negative number:)')
+print("Thank you!")
+print('\t\t\t','.'*20,'Program by AR','.'*20)
