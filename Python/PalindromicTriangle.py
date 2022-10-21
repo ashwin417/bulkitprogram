fafa837f91224b82d7c990e7ae0cf9fe5856b4eb
Leana Merin Ison <83947709+LEANA18@users.noreply.github.com>
2022-10-21 06:51:27 +0530

    Update PalindromicTriangle.py

--- Python/PalindromicTriangle.py
+++ Python/PalindromicTriangle.py
@@ -1,4 +1,4 @@
 n=int(input("Enter a positive integer: "))
-print("Palindromic triangle of size %d",n,"is")
+print("Palindromic triangle of size",n,"is:")
 for i in range (1,n):
     print((pow(10,i)//9)**2)
